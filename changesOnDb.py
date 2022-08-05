4f76228281164a010bd0e6275fffe38ef6030c05
redystum <rffarffarffa@gmail.com>
2022-08-05 15:59:49 +0100

    Search by id | Stable

--- changesOnDb.py
+++ changesOnDb.py
@@ -1,6 +1,11 @@
 import sqlite3
 import os
 
+"""
+file to make specific manual changes that are not part of the app.
+basically it is for testing
+"""
+
 roaming = os.getenv('APPDATA')
 
 path = roaming + '\\AnimeList\\data\\'
